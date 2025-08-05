72aabb83516ef838fef345e4be6aaf25ebb414e5
Deepak <ds262969@gmail.com>
2025-08-05 22:32:21 +0530

    Added the navbar text and buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 "use client";
 
 import FloatingSnippets from "@/components/FloatingSnippets";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <div className="relative w-full h-screen bg-[#131416]">
-      <FloatingSnippets />
+      <div className="absolute inset-0 z-">
+        <FloatingSnippets />
+      </div>
+      <div className="relative z-10">
+        <Navbar />
+      </div>
     </div>
   );
 }
