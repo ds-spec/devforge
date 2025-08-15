4d5a827746cd5b03e3bbcd988c30ef539d2cc4d5
Deepak <ds262969@gmail.com>
2025-08-15 11:31:16 +0530

    Made the animated gradient flow from top to input box

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 "use client";
 
-import FloatingSnippets from "@/components/FloatingSnippets";
-import Navbar from "@/components/Navbar";
 import SearchBar from "@/components/search-bar";
+
+// animate-[animatedgradient_2s_ease_infinite]
 
 export default function Home() {
   return (
     <div className="relative w-full h-screen shadow-inner backdrop-blur-3xl">
+      <div className="w-full h-full animate-[animatedgradient_1s_ease_infinite] top-0 absolute overflow-hidden">
+        <div className="w-full h-8  bg-gradient-to-l from-red-100 via-green-200 to-blue-300 blur-[80px]" />
+      </div>
       <SearchBar />
-    </div>  
+    </div>
   );
 }
